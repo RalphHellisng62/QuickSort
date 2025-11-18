e3920b6c96eb5ac3dd6d3f0928e0d54656128b25
RalphHellisng62 <arenasrafael889@gmail.com>
2025-11-18 15:52:51 -0600

    Se creo el parametro quick sort

--- OrdenamientoQuickSort.java
+++ OrdenamientoQuickSort.java
@@ -1,3 +1,11 @@
 public class OrdenamientoQuickSort {
     
+    public static void quickSort(int[] arr, int inicio, int fin) {
+        if (inicio < fin) {
+            int pi = partition(arr, inicio, fin);
+            quickSort(arr, inicio, pi -1);
+            quickSort(arr, pi + 1, fin);
+        }
+    }
+
 }
